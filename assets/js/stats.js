f20ad218f2909ebe6b0bc9cb1a019f323a270ea8
EALucero <ea.lucero@outlook.com>
2023-03-21 01:50:39 -0300

    problems

--- assets/js/stats.js
+++ assets/js/stats.js
@@ -1,9 +1,10 @@
 import API from "./data.js";
+import events from "./main.js";
 
 let data = await API();
-
 let attArr = [];
 let eveCap = [];
+let categories = [];
 
 data.events.forEach(e => {
   if (e.assistance == undefined) {
@@ -12,30 +13,34 @@ data.events.forEach(e => {
 
   attArr.push(e);
   eveCap.push(e);
+
+  if (!categories.includes(e.category)) {
+    categories.push(e.category);
+  }
 });
 
 let high = [];
 let low = [];
 
-let porcEv = (e) => {
+const porcEv = (e) => {
   let porc = (e.assistance * 100 / e.capacity);
   return parseFloat(porc).toFixed(2);
 }
 
 attArr.sort((a, b) => {
-  return (porcEv(a) > porcEv(b)? -1 : porcEv(a) < porcEv(b)? 1 : 0);
+  return (porcEv(a) > porcEv(b) ? -1 : porcEv(a) < porcEv(b) ? 1 : 0);
 })
 
 attArr.forEach(e => high.push(e));
 
 attArr.sort(function (a, b) {
-  return (porcEv(b) > porcEv(a)? -1 : porcEv(b) < porcEv(a)? 1 : 0);
+  return (porcEv(b) > porcEv(a) ? -1 : porcEv(b) < porcEv(a) ? 1 : 0);
 });
 
 attArr.forEach(e => low.push(e));
 
 eveCap.sort((a, b) => {
-  return (a.capacity > b.capacity? -1 : a.capacity < b.capacity? 1 : 0);
+  return (a.capacity > b.capacity ? -1 : a.capacity < b.capacity ? 1 : 0);
 })
 
 let eventList = document.getElementById("eventList");
@@ -73,4 +78,60 @@ eventList.innerHTML += `<tr>
             <td>${highRows}</td>
             <td>${lowRows}</td>
             <td>${capRows}</td>
-        </tr>`
+        </tr>`;
+
+let sumUpPrice = [];
+let sumPaPrice = 0;
+let upcomingEvents = events.cardProcess(data).upcomingEvents;
+let pastEvents = events.cardProcess(data).pastEvents;
+
+let upcoList = document.getElementById("upcoList");
+let catRows = "";
+
+categories.sort().forEach(cat => {
+  catRows += `<p>${cat}</p>`
+});
+
+const compArray = (a, b) => {
+  a.forEach((e, i) => {
+    console.log(e.category, b[i]);
+    if (e.category == b[i] && b[i] != undefined) {
+      sumUpPrice[i] += e.price;
+    }
+  })
+}
+
+compArray(upcomingEvents, categories);
+
+console.log(sumUpPrice);
+
+upcoList.innerHTML += `<tr>
+    <th colspan="3">Upcoming events statistics by category</th>
+  </tr>
+  <tr>
+    <td>Categories</td>
+    <td>Revenues</td>
+    <td>Percentage of attendance</td>
+  </tr>
+  <tr>
+    <td>${catRows}</td>
+    <td>${sumUpPrice}</td>
+    <td></td>
+  </tr>`;
+
+let pastList = document.getElementById("pastList");
+let pastRows = "";
+
+pastList.innerHTML += `<tr>
+    <th colspan="3">Past events statistics by category</th>
+  </tr>
+  <tr>
+    <td>Categories</td>
+    <td>Revenues</td>
+    <td>Percentage of attendance</td>
+  </tr>
+  <tr>
+    <td>${catRows}</td>
+    <td></td>
+    <td></td>
+  </tr>`;
